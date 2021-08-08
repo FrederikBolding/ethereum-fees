4f9723337675bb8848c3572360566585d68d22a2
Frederik Bolding <frederik.bolding@gmail.com>
2021-08-08 22:36:45 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Ethereum Fees",
+  },
+  plugins: ["gatsby-plugin-theme-ui", "gatsby-plugin-react-helmet"],
+};
